657fa94d00fc7b388fcbeb85c86aab766892f6f6
Jesse Smith <jesse.lee.smith@gmail.com>
2021-09-08 19:04:46 -0700

    added solution

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,31 @@
 # Implement your procedural solution here!
+def largest_palindrome_product()
+    first = 999
+    second = 999
+
+    palindromeProducts = []
+
+    while first > 900 && second > 900
+        product = first * second
+        if isPalindrome(product)
+            palindromeProducts.push(product)
+        end
+
+        if(second == 901)
+            first -= 1
+            second = first - 1
+        else
+            second -= 1
+        end
+    end
+
+    palindromeProducts.sort!
+
+    palindromeProducts.pop()
+end
+
+def isPalindrome(number)
+    string = number.to_s
+
+    string.eql?(string.reverse())
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,28 @@
 # Implement your object-oriented solution here!
+class LargestPalindromeProduct
+    attr_accessor :answer
+    def initialize()
+        first = 999
+        second = 999
+
+        palindromeProducts = []
+
+        while first > 900 && second > 900
+            product = first * second
+            if isPalindrome(product)
+                palindromeProducts.push(product)
+            end
+
+            if(second == 901)
+                first -= 1
+                second = first - 1
+            else
+                second -= 1
+            end
+        end
+
+        palindromeProducts.sort!
+
+        @answer = palindromeProducts.pop()
+    end
+end
